32c07b77620fe234a793539679f106d70bb63097
jordyn <jordynmarlow@gmail.com>
2021-02-20 17:10:03 -0500

    Buttons Open Dialog Boxes

--- MotorcycleDealership.py
+++ MotorcycleDealership.py
@@ -3,14 +3,102 @@ from PyQt5 import uic
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-class MotorcycleDealership(QMainWindow):
+class Timesheet(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('TimesheetDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+
+class Employees(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('EmployeesDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+        self.add_employee_bt.clicked.connect(self.openAddEmployees)
+    
+    def openAddEmployees(self):
+        #open AddEmployeeDialog.ui
+        dlg = AddEmployee()
+        dlg.exec_()
+
+class AddEmployee(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('AddEmployeeDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+
+class Advertisements(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('AdvertisementsDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+        self.add_advertisement_bt.clicked.connect(self.openAddAdvertisements)
+    
+    def openAddAdvertisements(self):
+        #open AddAdvertisementDialog.ui
+        dlg = AddAdvertisements()
+        dlg.exec_()
+
+class AddAdvertisements(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('AddAdvertisementDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+
+class NewOrder(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('NewOrderDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+
+class Inventory(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('InventoryDialog.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+
+class Homepage(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('MotorcycleDealership.ui', self)
+        self.setStyleSheet(open('Stylesheet.qss').read())
+        self.setWindowTitle('Motorcycle Dealership')
         self.show()
+        self.widgetInteractions()
+
+    def widgetInteractions(self):
+        self.timesheet_bt.clicked.connect(self.openTimesheet)
+        self.employees_bt.clicked.connect(self.openEmployees)
+        self.ads_bt.clicked.connect(self.openAds)
+        self.new_order_bt.clicked.connect(self.openNewOrder)
+        # when a widget in inventory_scroll_area is clicked, connect to self.openInventory
+
+    def openTimesheet(self):
+        #open TimeSheetDialog.ui
+        dlg = Timesheet()
+        dlg.exec_()
+
+    def openEmployees(self):
+        #open EmployeesDialog.ui
+        dlg = Employees()
+        dlg.exec_()
+
+    def openAds(self):
+        #open AdvertisementsDialog.ui
+        dlg = Advertisements()
+        dlg.exec_()
+
+    def openNewOrder(self):
+        #open NewOrderDialog.ui
+        dlg = NewOrder()
+        dlg.exec_()
+
+    def openInventory(self):
+        #open InventoryDialog.ui
+        dlg = Inventory()
+        dlg.exec_()
 
 app = QApplication(sys.argv)
-window = MotorcycleDealership()
-window.setStyleSheet(open('Stylesheet.qss').read())
+window = Homepage()
 window.show()
 sys.exit(app.exec_())
