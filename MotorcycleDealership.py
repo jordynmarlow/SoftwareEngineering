baaa35dea6dd69c467e54f23fc0c20fff74ddf69
jordyn <jordynmarlow@gmail.com>
2021-03-03 13:33:41 -0500

    Inventory and Orders list functionality
    Orders list still needs work but it's dependent on other tasks

--- MotorcycleDealership.py
+++ MotorcycleDealership.py
@@ -1,8 +1,56 @@
-import sys
+import sys, sqlite3
 from PyQt5 import uic
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from random import *
+from sqlite3 import Error
+
+sql_connection = None
+
+def connect_to_db():
+    try:
+        sql_connection = sqlite3.connect('MotorDB.db')  # Try to connect to the database
+    except Error as e:  # ...Except if there is an sqlite3 error,
+        print(e)  # print the error
+    return sql_connection.cursor()
+
+class InventoryDatabase():
+    def __init__(self):
+        self.cursor = connect_to_db()
+    
+    def get_all_inventory_items(self):
+        self.cursor.execute('SELECT * FROM \
+                                (SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM ProductsInventory I UNION\
+                                SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM PartsInventory I UNION\
+                                SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM MerchandiseInventory I)')
+        return self.cursor.fetchall()
+
+    def get_fields(self, item_no):
+        self.cursor.execute('SELECT * FROM \
+                                (SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM ProductsInventory I UNION\
+                                SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM PartsInventory I UNION\
+                                SELECT I.ITEM_NUMBER, I.PRICE, I.QUANTITY FROM MerchandiseInventory I) I\
+                            WHERE I.ITEM_NUMBER=\'%s\'' % (item_no))
+        return self.cursor.fetchall()[0]
+
+class OrdersDatabase():
+    def __init__(self):
+        self.cursor = connect_to_db()
+    
+    def get_all_orders(self):
+        self.cursor.execute('SELECT * FROM \
+                                (SELECT O.ORDER_ID, O.DATE FROM WorkOrders O UNION\
+                                SELECT O.ITEM_NUMBER, O.DATE FROM BikeOrders O UNION\
+                                SELECT O.ITEM_NUMBER, O.DATE FROM MerchandiseOrders O)')
+        return self.cursor.fetchall()
+
+    def get_fields(self, item_no):
+        self.cursor.execute('SELECT * FROM \
+                                (SELECT O.ORDER_ID, O.DATE FROM WorkOrders O UNION\
+                                SELECT O.ITEM_NUMBER, O.DATE FROM BikeOrders O UNION\
+                                SELECT O.ITEM_NUMBER, O.DATE FROM MerchandiseOrders O) O\
+                            WHERE O.ITEM_NUMBER=\'%s\'' % (item_no))
+        return self.cursor.fetchall()[0]
 
 class Timesheet(QDialog):
     def __init__(self):
@@ -57,12 +105,22 @@ class Inventory(QDialog):
         super().__init__()
         uic.loadUi('InventoryDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
-
-class ItemButton(QWidget):
+    
+    def populateFields(self, fields):
+        self.item_no_lbl.setText(fields[0])
+        #self.name_lbl.setText()
+        self.price_lbl.setText(fields[1])
+        self.quantity_lbl.setText(fields[2])
+    
+class Order(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('itemButton.ui', self)
+        uic.loadUi('OrderDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
+    
+    def populateFields(self, fields):
+        # populate fields in OrderDialog.ui
+        pass
 
 class Homepage(QMainWindow):
     def __init__(self):
@@ -71,6 +129,8 @@ class Homepage(QMainWindow):
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.setWindowTitle('Motorcycle Dealership')
         self.show()
+        self.populateInventoryList()
+        self.populateOrdersList()
         self.widgetInteractions()
 
     def widgetInteractions(self):
@@ -78,8 +138,22 @@ class Homepage(QMainWindow):
         self.employees_bt.clicked.connect(self.openEmployees)
         self.ads_bt.clicked.connect(self.openAds)
         self.new_order_bt.clicked.connect(self.openNewOrder)
-        self.wip_bt.clicked.connect(self.create_inventory_item)
-        # when a widget in inventory_scroll_area is clicked, connect to self.openInventory
+        self.inventory_list.itemClicked.connect(self.openInventoryItem)
+        # self.orders_list.itemClicked.connect(self.openOrder)
+    
+    def populateInventoryList(self):
+        self.inventory_list.setVerticalScrollBar(QScrollBar(self))
+        self.inventory = InventoryDatabase()
+        items = self.inventory.get_all_inventory_items()
+        for item in items:
+            self.inventory_list.addItem(QListWidgetItem('\t\t\t'.join(item)))
+    
+    def populateOrdersList(self):
+        self.orders_list.setVerticalScrollBar(QScrollBar(self))
+        self.orders = OrdersDatabase()
+        items = self.orders.get_all_orders()
+        for item in items:
+            self.orders_list.addItem(QListWidgetItem('\t\t'.join(item)))
 
     def openTimesheet(self):
         #open TimeSheetDialog.ui
@@ -101,24 +175,17 @@ class Homepage(QMainWindow):
         dlg = NewOrder()
         dlg.exec_()
 
-    def openInventory(self):
+    def openInventoryItem(self, item):
         #open InventoryDialog.ui
         dlg = Inventory()
+        dlg.populateFields(self.inventory.get_fields(item.text().split('\t')[0]))
         dlg.exec_()
-
-    def create_inventory_item(self):
-        b1 = QPushButton("new")
-        b2 = QPushButton("New")
-        orders = self.order_layout.layout()
-        if orders is None:
-            print ("Could not get order layout")
-            return None
-        orders.insertWidget(0, b1)
-        items = self.inventory_layout.layout()
-        if items is None:
-            print ("Could not get inventory layout")
-            return None
-        items.insertWidget(0, b2)
+    
+    def openOrder(self, item):
+        #open OrderDialog.ui
+        dlg = Order()
+        dlg.populateFields(self.orders.get_fields(item.text().split('\t')[0]))
+        dlg.exec_()
 
 def CheckFormatCard(credNum):
     '''for x in range(16):
