1cbbb9f62af915cbc4055f4d60a7a225e32ef66d
Alexander McCarthy <KnufWons@users.noreply.github.com>
2021-03-05 17:14:13 -0500

    Files moved & references changed

--- MotorcycleDealership.py
+++ MotorcycleDealership.py
@@ -6,20 +6,21 @@ from random import *
 
 # constants
 CONFIG_FILE = 'config.ini'
+UI_PATH = './UI/'
 
 class Timesheet(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('TimesheetDialog.ui', self)
+        uic.loadUi(UI_PATH + 'TimesheetDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
 
 class Employees(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('EmployeesDialog.ui', self)
+        uic.loadUi(UI_PATH + 'EmployeesDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.add_employee_bt.clicked.connect(self.openAddEmployees)
-    
+
     def openAddEmployees(self):
         #open AddEmployeeDialog.ui
         dlg = AddEmployee()
@@ -28,16 +29,16 @@ class Employees(QDialog):
 class AddEmployee(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('AddEmployeeDialog.ui', self)
+        uic.loadUi(UI_PATH + 'AddEmployeeDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
 
 class Advertisements(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('AdvertisementsDialog.ui', self)
+        uic.loadUi(UI_PATH + 'AdvertisementsDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.add_advertisement_bt.clicked.connect(self.openAddAdvertisements)
-    
+
     def openAddAdvertisements(self):
         #open AddAdvertisementDialog.ui
         dlg = AddAdvertisements()
@@ -46,16 +47,16 @@ class Advertisements(QDialog):
 class AddAdvertisements(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('AddAdvertisementDialog.ui', self)
+        uic.loadUi(UI_PATH + 'AddAdvertisementDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
-        
+
 class AddPayment(QDialog): # possibly in openAddPayment functions from new orders, send in the price as well (Phil)
     def __init__(self):
         super().__init__()
-        uic.loadUi('AddPaymentDialog.ui', self)
+        uic.loadUi(UI_PATH + 'AddPaymentDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.get_rate_bt.clicked.connect(self.getInterest)
-        
+
     def getInterest(self):
         credNum = self.credit_card_text.toPlainText()
         ssnNum = self.ssn_text.toPlainText()
@@ -63,20 +64,20 @@ class AddPayment(QDialog): # possibly in openAddPayment functions from new order
             self.message_lbl.setText("Interest generated and saved!")
             self.interest_rate_lbl.setText(GenerateInterest(credNum, ssnNum))
             #self.get_rate_bt.hide()
-        else: 
+        else:
             self.message_lbl.setText("Wrong format for Credit Card or SSN! Please double check them and try again.")
             self.interest_rate_lbl.setText("")
-            
+
 class NewOrder(QDialog):
     def __init__(self, homepage):
         super().__init__()
         self.homepage = homepage
         self.mechanics = ['mechanic1', 'mechanic2'] # change this list to pull from all mechanics in db
-        uic.loadUi('NewWorkOrderDialog.ui', self)
+        uic.loadUi(UI_PATH + 'NewWorkOrderDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.add_payment_bt.clicked.connect(self.openAddPayment)
         self.reassign_mechanic_bt.clicked.connect(self.reassignMechanic)
-        
+
     def openAddPayment(self):
         #open AddPaymentDialog.ui
         dlg = AddPayment()
@@ -91,13 +92,13 @@ class NewOrder(QDialog):
 class Inventory(QDialog):
     def __init__(self):
         super().__init__()
-        uic.loadUi('InventoryDialog.ui', self)
+        uic.loadUi(UI_PATH + 'InventoryDialog.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
 
 class Homepage(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('MotorcycleDealership.ui', self)
+        uic.loadUi(UI_PATH + 'MotorcycleDealership.ui', self)
         self.setStyleSheet(open('Stylesheet.qss').read())
         self.setWindowTitle('Motorcycle Dealership')
         self.show()
@@ -110,7 +111,7 @@ class Homepage(QMainWindow):
         self.new_order_bt.clicked.connect(self.openNewOrder)
         self.change_pin_bt.clicked.connect(self.changePIN)
         # when a widget in inventory_scroll_area is clicked, connect to self.openInventory
-    
+
     def getPIN(self):
         pin, ok = QInputDialog.getText(self, 'Manager PIN', 'Enter your PIN:')
         parser = configparser.ConfigParser()
@@ -118,7 +119,7 @@ class Homepage(QMainWindow):
         while ok and pin != parser['DEFAULT']['ManagerPIN']:
             pin, ok = QInputDialog.getText(self, 'Manager Access Only', 'Incorrect PIN. Please try again.')
         return ok
-    
+
     def changePIN(self):
         if self.getPIN():
             new_pin, ok = QInputDialog.getText(self, 'Manager PIN', 'Enter your new PIN:')
@@ -158,11 +159,11 @@ def CheckFormatCard(credNum):
     '''for x in range(16):
         credNum = credNum_s + str(randint(0,9))
     print("Card String: " + credNum)
-    
+
     formatCred = credNum[0:4] + "-" + credNum[4:8] + "-" + credNum[8:12] + "-" + credNum[12:16]
     print("Formatted: " + formatCred)'''
     checkCard = True
-    
+
     if (len(credNum) == 19): # correct length
         #xxxx-xxxx-xxxx-xxxx
         if (credNum[4] != "-" or credNum[9] != "-" or credNum[14] != "-"): # correct dash spots
@@ -174,9 +175,9 @@ def CheckFormatCard(credNum):
                     int(num)
                 except ValueError:
                     checkCard = False
-            
+
     else: checkCard = False
-            
+
     return checkCard
 
 def CheckFormatSSN(ssnNum):
@@ -187,12 +188,12 @@ def CheckFormatSSN(ssnNum):
     formatSSN = ssnNum[0:3] + "-" + ssnNum[3:5] + "-" + ssnNum[5:9]
     print("Formatted: " + formatSSN)'''
     checkSSN = True
-    
+
     if (len(ssnNum) == 11): # correct length
         #xxx-xx-xxxx
         if (ssnNum[3] != "-" or ssnNum[6] != "-"): # correct dash spots
             checkSSN = False
-            
+
         else: # only integers
             noDash = ssnNum.split("-")
             for num in noDash:
@@ -200,11 +201,11 @@ def CheckFormatSSN(ssnNum):
                     int(num)
                 except ValueError:
                     checkSSN = False
-            
+
     else: checkSSN = False
-            
+
     return checkSSN
-    
+
 def GenerateInterest(credNum, ssnNum):
     ''' random interest rate
     intRate = round(uniform(3,21), 2)
@@ -215,10 +216,10 @@ def GenerateInterest(credNum, ssnNum):
     ssnNoDash = ssnNum.split("-")
     finalC = 1
     finalS = 1
-    
+
     for cNum in credNoDash:
         finalC += finalC * int(cNum) + int(cNum)/10000
-        
+
     for sNum in ssnNoDash:
         finalS += finalS * int(sNum) + int(sNum)/10000
 
